1a97707de1247584f928a0769b075d3f350f2913
DJE-2006 <dhruvjae.guboc@hcdc.edu.ph>
2026-05-03 16:51:24 +0800

    Update member name formatting

--- app/index.tsx
+++ app/index.tsx
@@ -9,8 +9,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text>Group Members:</Text>
       <Text>Joshua Ralph Tamparong</Text>
-      <Text>Dhruv Jae Guboc</Text>
+      <Text>Dhruv Jae E. Guboc</Text>
+      <Text>NewMembers</Text>
       <Text>BSIT-2A</Text>
       <Text>Exercise 9: Group Project</Text>
     </View>
