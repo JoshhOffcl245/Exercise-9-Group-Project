3bb8fd97722e2d197c21b9f2c8141f27a497d82a
JoshhOffcl245 <joshuaralph.tamparong@hcdc.edu.ph>
2026-05-03 17:00:03 +0800

    Update app/index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -9,9 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Joshua Ralph Tamparong</Text>
-      <Text>Dhruv Jae Guboc</Text>
-      <Text>Paulo Gabriel Limit</Text>
+      <Text>1. Joshua Ralph Tamparong</Text>
+      <Text>2. Dhruv Jae Guboc</Text>
+      <Text>3. Paulo Gabriel Limit</Text>
       <Text>BSIT-2A</Text>
       <Text>Exercise 9: Group Project</Text>
     </View>
